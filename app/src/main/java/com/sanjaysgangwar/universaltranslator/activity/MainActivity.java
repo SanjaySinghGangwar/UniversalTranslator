06a4d39992adc5fa29fd973ab331e3bfae1263de
hgnis <gangwar.jar@gmail.com>
2020-08-29 11:48:51 +0530

    init

---
+++ app/src/main/java/com/sanjaysgangwar/universaltranslator/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sanjaysgangwar.universaltranslator.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sanjaysgangwar.universaltranslator.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
